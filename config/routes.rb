27a004a8fe60f9b71fa3217af29e7660cc7ac86b
pquezadam <pquezada2009@hotmail.es>
2020-01-30 20:54:23 -0300

    agregando home

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  
-  
+root 'home#index'
+  get 'home/index'
     devise_for :users, controllers: {
-      sessions: 'users/sessions'
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
     }
-  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+end
