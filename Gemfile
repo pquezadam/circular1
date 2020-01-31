ede547acc376862c326ca609949d040585ca25d7
pquezadam <pquezada2009@hotmail.es>
2020-01-31 18:54:51 -0300

    agregando seed

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'pg', '>= 0.18', '< 2.0'
 gem 'devise'
 gem 'geocoder'
 gem 'gmaps4rails'
+gem 'faker'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+addresses = "Emporio La Granel Av. Salvador 757, Providencia
+Un Cuarto a Granel Moneda 3156, Santiago
+Espacio Granel, Las Condes
+Kulko, Las Condes
+Sin Envase  Irarrázaval 3620, Ñuñoa
+Circularshop, Chicureo
+Cococacao, Manuel Rodríguez 3150, Maipú
+MiaSoul Market Nueva Providencia 2060 Local 8 Providencia
+Mundo Natural Paseo Los Trapenses, Lo Barnechea
+La Balanza Antupiren 9301, Local21, Peñalolén
+Alma Zero Av. Apoquindo 7942, Torre 1, Loc. 8, Las Condes"
+
+Store.destroy_all
+addresses = addresses.split("\n")
+addresses.each do |addresses|
+    Store.create!(name: Faker::Name.name, address: addresses)
+    sleep 1
+end
