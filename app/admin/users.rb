2b0dd1dbb2dff21a75dc6eeffbd7f2eac963b3d8
pquezadam <pquezada2009@hotmail.es>
2020-02-09 16:26:08 -0300

    modificando active-user

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -38,4 +38,14 @@ form do |f|
   actions
 end
 
+controller do
+  def update
+    if (params[:user][:password].blank? && params[:user][:password_confirmation].blank?)
+      params[:user].delete("password")
+      params[:user].delete("password_confirmation")
+    end
+    super
+  end
+end
+
 end
